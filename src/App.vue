d14edd74e52d28ae6e2b284e366f42ae59e317cc
NKUmianman <1277463846@qq.com>
2023-06-28 11:30:39 +0800

    git test"

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <h3>模板语法</h3>
+    <p>{{ rawhtml }}</p>
+    <p v-html="rawhtml"></p>
+    <p @click="increment">{{ count }}</p>
+</template>
+
+<script>
+import { nextTick } from "vue";
+
+export default {
+    data() {
+        return {
+            rawhtml: "<span style=\"color: red;\">nihao</span>",
+            count: 1,
+        };
+    },
+    methods: {
+        increment() {
+            this.count++;
+            nextTick(() => {
+                this.count++;
+            });
+        },
+    },
+    mounted() {
+        // this.increment();
+    },
+};
+</script>
